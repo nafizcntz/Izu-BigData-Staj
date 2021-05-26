73c7f4cac127dff13038393d8cc5c1db23e9b7df
Muhammed Nafiz Canıtez <nafizcntz@hotmail.com>
2021-05-27 02:09:44 +0300

    Son

--- JavaScript/data.js
+++ JavaScript/data.js
@@ -1,18 +1,17 @@
+let yas_adet_lbl = [];
+let yas_lbl = [];
+let yas_sozluk = [];
+let okul_lbl = [];
+let okul_sozluk = [];
+let okul_adet_sozluk = [];
+
 let ctx = document.getElementById('myChart').getContext('2d');
 let ctx2 = document.getElementById('myChart2').getContext('2d');
 let ctx3 = document.getElementById('myChart3').getContext('2d');
-let ageCountLabel = [];
-let ageLabel = [];
-let ageDict = [];
-let schoolLabel = [];
-let schoolDict = [];
-let schoolCountLabel = [];
-
-var mymap = L.map('mapid').setView([0,0], 11);
-const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
-const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileUrl,{attribution});
-tiles.addTo(mymap);
+let mymap = L.map('mapid').setView([0,0], 10.7);
+let tile_link = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+let tile_layer = L.tileLayer(tile_link);
+tile_layer.addTo(mymap);
 
 
     let url = 'https://us-central1-outsmartooh-demo.cloudfunctions.net/data';
@@ -20,20 +19,20 @@ tiles.addTo(mymap);
                 .then(function (response) {
                     return response.json();
                 })
-                .then(function (data) {
+                .then(function (output) {
                     
-                    var male = 0;
-                    var female = 0;
+                    let erkek_sayi = 0;
+                    let kadin_sayi = 0;
 
-                    for (var i = 0; i < data.data.length; i++) {
-                        if(data.data[i].gender == "M") {male++;}
-                        else {female++};
+                    for (let i = 0; i < output.data.length; i++) {
+                        if(output.data[i].gender == "M") {erkek_sayi++;}
+                        else {kadin_sayi++};
                     }
                     new Chart(ctx, {
                         type: 'pie',
                         data: {
                           datasets: [{
-                            data: [male, female],
+                            data: [erkek_sayi, kadin_sayi],
                             backgroundColor: ['rgb(54, 162, 235, 0.7)', 'rgb(255, 99, 132, 0.7)'],
                           }],
                           hoverOffset: 4,
@@ -47,31 +46,31 @@ tiles.addTo(mymap);
                       });
 
 
-                      let schoolFlag = false;
-                      for (var i = 0; i < data.data.length; i++) {
-                          schoolFlag = false;
-                          for (var j = 0; j < i; j++) {
-                              if(schoolLabel[j] == data.data[i].edu) {schoolCountLabel[j] += 1; schoolFlag = true; break; }
+                      let okul_gecici = false;
+                      for (let i = 0; i < output.data.length; i++) {
+                          okul_gecici = false;
+                          for (let j = 0; j < i; j++) {
+                              if(okul_lbl[j] == output.data[i].edu) {okul_adet_sozluk[j] += 1; okul_gecici = true; break; }
                               
                           }
-                          if(!schoolFlag) {schoolLabel[i] = data.data[i].edu; schoolCountLabel[i] = 1;}
+                          if(!okul_gecici) {okul_lbl[i] = output.data[i].edu; okul_adet_sozluk[i] = 1;}
                       }
-                      for (var i = 0; i < schoolLabel.length; i++) {
-                          if(schoolLabel[i] != undefined){
-                              schoolDict[schoolLabel[i]] = schoolCountLabel[i];
+                      for (let i = 0; i < okul_lbl.length; i++) {
+                          if(okul_lbl[i] != undefined){
+                              okul_sozluk[okul_lbl[i]] = okul_adet_sozluk[i];
                           }
                       }
-                      schoolDict = Object.keys(schoolDict).sort().reduce((r, k) => (r[k] = schoolDict[k], r), {});
+                      okul_sozluk = Object.keys(okul_sozluk).sort().reduce((r, k) => (r[k] = okul_sozluk[k], r), {});
                       new Chart(ctx2, {
                           type: 'bar',
                           data: {
   
                               datasets: [{
-                              data: Object.values(schoolDict),
+                              data: Object.values(okul_sozluk),
                               backgroundColor:['rgb(255, 205, 86, 0.7)',"rgb(0, 153, 255, 0.7)"],
                               
                               }],
-                              labels: Object.keys(schoolDict)
+                              labels: Object.keys(okul_sozluk)
                           },
                           options: {
                             legend: {
@@ -86,29 +85,29 @@ tiles.addTo(mymap);
                           }
                           }); 
 
-                    let ageFlag = false;
-                    ageDict = Object.keys(ageDict).sort().reduce((r, k) => (r[k] = ageDict[k], r), {});
-                    for (var i = 0; i < data.data.length; i++) {
-                        ageFlag = false;
-                        for (var j = 0; j < i; j++) {
-                            if(ageLabel[j] == data.data[i].age) {ageCountLabel[j] += 1; ageFlag = true; break; }
+                    let yas_gecici = false;
+                    yas_sozluk = Object.keys(yas_sozluk).sort().reduce((r, k) => (r[k] = yas_sozluk[k], r), {});
+                    for (let i = 0; i < output.data.length; i++) {
+                        yas_gecici = false;
+                        for (let j = 0; j < i; j++) {
+                            if(yas_lbl[j] == output.data[i].age) {yas_adet_lbl[j] += 1; yas_gecici = true; break; }
                         }
-                        if(!ageFlag) {ageLabel[i] = data.data[i].age; ageCountLabel[i] = 1;}
+                        if(!yas_gecici) {yas_lbl[i] = output.data[i].age; yas_adet_lbl[i] = 1;}
                     }
-                    for (var i = 0; i < ageLabel.length; i++) {
-                        ageDict[ageLabel[i]] = ageCountLabel[i];
+                    for (let i = 0; i < yas_lbl.length; i++) {
+                        yas_sozluk[yas_lbl[i]] = yas_adet_lbl[i];
 
                     }
                     new Chart(ctx3, {
                         type: 'bar',
                         data: {
                             datasets: [{
-                            data: Object.values(ageDict),
+                            data: Object.values(yas_sozluk),
                             backgroundColor:["rgb(101, 235, 93, 0.7)", "rgb(150, 126, 34, 0.7)", "rgb(248, 167, 75, 0.7)", 
                                              "rgb(202, 206, 70, 0.7)", "rgb(2, 207, 216, 0.7)", "rgb(171, 91, 222, 0.7)", "rgb(109, 239, 87, 0.7)", "rgb(64, 59, 99, 0.7)"],
                             
                             }],
-                            labels: Object.keys(ageDict)
+                            labels: Object.keys(yas_sozluk)
                         },
                         
                         options: {
@@ -125,29 +124,27 @@ tiles.addTo(mymap);
                         }
                         });
 
-                        var meanLatitude = 0;
-                        var meanLongitude = 0;
-    
-                        for (var i = 0; i < data.data.length; i++) {
-                            var latitude = data.data[i].geo[0];
-                            var longitude = data.data[i].geo[1];
+                        let latitude_ort = 0, longitude_ort = 0;
+
+                        for (let i = 0; i < output.data.length; i++) {
+                            let latitude = output.data[i].geo[0];
+                            let longitude = output.data[i].geo[1];
                             
-                            meanLatitude += latitude;
-                            meanLongitude += longitude;
+                            latitude_ort += latitude;
+                            longitude_ort += longitude;
     
                             L.marker([latitude,longitude]).addTo(mymap);
                          }
     
-                         meanLatitude /= data.data.length;
-                         meanLongitude /= data.data.length;
-    
-                         mymap.panTo([meanLatitude,meanLongitude]);
+                         latitude_ort = latitude_ort / output.data.length;
+                         longitude_ort = longitude_ort / output.data.length;
+                         mymap.panTo([latitude_ort,longitude_ort]);
     
                         
 
                 })
                 .catch(function (err) {
-                    console.log('error: ' + err);
+                    console.log(err);
                 });
 
 
